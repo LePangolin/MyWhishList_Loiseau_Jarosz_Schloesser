1ad130341fbdbcde8f6c1ffa7542288277e5979b
LePangolin <91209500+LePangolin@users.noreply.github.com>
2022-01-15 00:26:19 +0100

    Update VueListes.php

--- src/Vue/VueListes.php
+++ src/Vue/VueListes.php
@@ -2,6 +2,7 @@
 namespace wishlist\Vue;
 
 use Slim\Container;
+use wishlist\Authentificateur\Authentication;
 use wishlist\Controlleur\ControlleurItems;
 use wishlist\Controlleur\ControlleurListes;
 
@@ -68,14 +69,31 @@ class VueListes{
                     }
                 }
             }
-
-
+            
+            Authentication::init();
+            $pdo = Authentication::$connexion;
+            $query = "Select userid from liste where token = ?";
+            $st = $pdo->prepare($query)->execute([$token]);
+            $row = $st->fetch(PDO::FETCH_ASSOC);
+            $uid = $row['userid'];
+            if(isset($_SESSION['profile'])){
+                if($uid == $_SESSION['profile']['userid']){
+                    $res = <<<END
+                    <container>
+                        <p>$infoListe $ph</p>
+                        <p><a class='btn btn-info text-light' href="$tabUrl[1]" role='button'>Voir un autre exemple de liste</a></p>
+                        <p><a class='btn btn-info text-light' href="" role='button'>Ajouter un item à la liste</a></p> 
+                    </container>
+                    END;
+                }
+            }else{
             $res = <<<END
                 <container>
                     <p>$infoListe $ph</p>
                     <p><a class='btn btn-info text-light' href="$tabUrl[1]" role='button'>Voir un autre exemple de liste</a></p>
                 </container>
                 END;
+            }
         }
 
         return($vue->getNav().$res.$vue->getFooter());
