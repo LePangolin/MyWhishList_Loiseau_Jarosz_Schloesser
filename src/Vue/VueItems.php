0f824887013e9532a8e60bf826fd606fcd93cca7
volress <hug0.loiesau@gmail.com>
2022-01-15 14:16:28 +0100

    Affichage nom + commentaire reservation

--- src/Vue/VueItems.php
+++ src/Vue/VueItems.php
@@ -4,6 +4,7 @@ namespace wishlist\Vue;
 
 use Slim\Container;
 use wishlist\Controlleur\ControlleurItems as ControlleurItems;
+use wishlist\Controlleur\ControlleurListes;
 use wishlist\Models\Item;
 
 class VueItems{
@@ -30,6 +31,7 @@ class VueItems{
             }
         } else {
             $tab_r=ControlleurItems::toutReservation();
+            $tab_l=ControlleurListes::toutListes();
             $estReserve=false;
             foreach($tab_v as $it){
                 if($it->id == $id){
@@ -39,31 +41,39 @@ class VueItems{
                     foreach ($tab_r as $reservation){
                         if ($reservation->iditem == $it->id){
                             $estReserve=true;
+                            $nom=$reservation->nom;
+                            $commentaire=$reservation->commentaire;
                         }
                     }
                     if(!$estReserve){
                         $ph.="<h1>Faire le formulaire</h1>";
                     } else {
-                        $ph.="<p>L'item est déjà réservé</p>";
+                        /*la date d'échéance est passée et
+                        l'utilisateur est le créateur de la liste on affiche les infos de la reservation*/
+                        foreach ($tab_l as $liste){
+                            if($liste->no == $it->liste_id){
+                                if(isset($_SESSION["profile"])){
+                                    if($_SESSION["profile"]["userid"] == $liste->user_id && $liste->expiration<date("F j, Y, a")){
+                                        $ph.="<p>$nom $commentaire</p>";
+                                    } else {
+                                        $ph.="<p>L'item est déjà réservé</p>";
+                                    }
+                                }
+                            }
+                        }
+
                     }
                 }
             }
 
         }
 
-        return($vue->getNav()."<!DOCTYPE html>
-                    <html>
-                        <head>
-                            <meta charset=\"UTF-8\">
-                            <title>Liste des items</title>
-                        </head>
-                        <body>
+        return($vue->getNav()."
                                 <h1>Items</h1>
                         
                                 <p>$ph</p>
                                 
                                 <a class='btn btn-info text-light' href=$tabUrl[1] role=button>Retour à la liste</a>
-                        </body>
                      </html>".$vue->getFooter());
     }
 }
